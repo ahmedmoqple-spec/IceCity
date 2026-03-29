33f5c24c26e2bb9cc27c693eab719d75831c9c59
ahmed moqble <ahmedmoqple@gmail.com>
2026-03-29 07:50:09 +0200

    Week4-Beginner-SOLID-DesignPatterns

--- Program.cs
+++ Program.cs
@@ -5,57 +5,26 @@ using System.Threading.Tasks;
 
 class Program
 {
-    static async Task Main(string[] args)
+    static void Main(string[] args)
     {
+        Console.WriteLine("--- IceCity: Week 4 SOLID & Design Patterns ---\n");
+
         var house = new House();
-        house.Heaters.Add(new ElectricHeater { PowerKw = 1500 });
+        house.Heaters.Add(new ElectricHeater { PowerKw = 2000 });
+        house.Heaters.Add(new SolarHeater { PowerKw = 1500 }); 
 
-        SaveDailyUsageDelegate saveDelegate = usage => house.DailyUsages.Add(usage);
+        house.DailyUsages.Add(new DailyUsage { Date = DateTime.UtcNow, HoursWorked = 50, HeaterValue = 2000 });
+        house.DailyUsages.Add(new DailyUsage { Date = DateTime.UtcNow.AddDays(-1), HoursWorked = 60, HeaterValue = 1800 });
 
-        var heater = house.Heaters[0];
-        if (heater != null)
-        {
-            heater.OpenHeater += (s, e) => Console.WriteLine($"[Event] Heater {((HeaterBase)s!).Id} Opened.");
-            heater.CloseHeater += (s, e) => 
-            {
-                var h = (HeaterBase)s!;
-                var usage = new DailyUsage { Date = DateTime.UtcNow.Date, HoursWorked = e.DurationHours, HeaterValue = h.PowerKw };
-                saveDelegate(usage);
-                Console.WriteLine($"[Event] Heater Closed. Usage saved!");
-            };
-        }
+        ICostStrategyFactory factory = new CostStrategyFactory();
+        ICostService costService = new CostService(factory);
 
-        try
-        {
-            if (house.Heaters[0] != null)
-            {
-                house.Heaters[0]!.Open();
-                throw new HeaterFailedException("Heater exploded!"); 
-            }
-        }
-        catch (HeaterFailedException ex)
-        {
-            Console.WriteLine($"\n[Error] {ex.Message}");
-            var newHeater = await CityCenterService.RequestReplacementAsync(house.HouseId, house.Heaters[0]!.Id);
-            house.Heaters[0] = null;
-            house.Heaters[0] = newHeater;
-        }
+        double standardCost = costService.CalculateHouseCost(house, "Standard");
+        Console.WriteLine($"Cost using Standard Strategy: {standardCost:F2}");
 
-        if (house.Heaters[0] != null)
-        {
-            house.Heaters[0]!.Close();
-        }
+        double ecoCost = costService.CalculateHouseCost(house, "Eco");
+        Console.WriteLine($"Cost using Eco Strategy: {ecoCost:F2}");
 
-        var weatherService = new WeatherService();
-        var apiData = await weatherService.GetLastMonthWeatherAsync();
-        house.DailyUsages.AddRange(apiData);
-
-        var printService = new PrintingService();
-        printService.PrintLastMonthDailyUsageWithThreads(house);
-        await printService.PrintLastMonthDailyUsageWithTasks(house);
-
-        var service1 = new Service1();
-        double median = service1.CalculateMedianHeaterValue(house.DailyUsages);
-        Console.WriteLine($"\nMedian Heater Value: {median}");
+        Console.WriteLine("\nAll Design Patterns and SOLID Principles applied successfully!");
     }
 }
